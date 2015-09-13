28784f9f2266f15880f9f43a53ca8a5cd034454c
freedom <stefano.vettor@mac.com>
2015-09-13 12:54:10 +0200

    Initial Commit

---
+++ MyJSON/MyJSON.h
@@ -0,0 +1,19 @@
+//
+//  MyJSON.h
+//  MyJSON
+//
+//  Created by Stefano Vettor on 13/09/15.
+//  Copyright © 2015 Stefano Vettor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyJSON.
+FOUNDATION_EXPORT double MyJSONVersionNumber;
+
+//! Project version string for MyJSON.
+FOUNDATION_EXPORT const unsigned char MyJSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyJSON/PublicHeader.h>
+
+
